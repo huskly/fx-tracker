90885742b6fd35ae1733e6c2ee4e57b8fced34c8
Felipe Lima <felipe.lima@gmail.com>
2026-06-23 09:11:45 -0700

    chore: Add ci deploy scripts via gh actions

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const base = process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : "/";
+
 export default defineConfig({
+  base,
   plugins: [react()],
 });
